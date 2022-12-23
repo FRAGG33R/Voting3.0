9d1d2319e407e6c2341a97a24684b4c5bd14f0cf
Abdelkarim Er-razki <aer-razk@e1r13p5.1337.ma>
2022-12-23 21:39:21 +0100

    checking wheter the hash is empty and emiting an evnt

--- VotingContract/test/test.js
+++ VotingContract/test/test.js
@@ -1,28 +1,39 @@
-const NftVoting = artifacts.require('NftVoting');
+const NftVoting = artifacts.require("NftVoting");
 
-contract('NftVoting', (accounts) => {
-  let contract;
+contract("NftVoting", accounts => {
+  it("should allow voting for a valid NFT", async () => {
+    const contract = await NftVoting.new();
+    const nftHash = "0xabcdef0123456789abcdef0123456789";
 
-  beforeEach(async () => {
-    contract = await NftVoting.new();
+    await contract.vote(nftHash);
+
+    const votes = await contract.getNumberVotes(nftHash);
+    assert.equal(votes, 1, "The NFT should have 1 vote");
   });
 
-  it('should increment vote count for NFT', async () => {
-    const nftHash = '0x1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef';
-    await contract.vote(nftHash);
-    const voteCount = await contract.getNumberVotes(nftHash);
-    assert.equal(voteCount, 1, 'incorrect vote count');
-  });
+  it("should not allow voting for an empty NFT hash", async () => {
+    const contract = await NftVoting.new();
+    const nftHash = "0x0000000000000000000000000000000000000000";
 
-  it('should prevent a voter from voting more than once', async () => {
-    const nftHash = '0x0987654321fedcba0987654321fedcba0987654321fedcba0987654321fedcba';
-    await contract.vote(nftHash);
     try {
       await contract.vote(nftHash);
-      assert.fail('should have thrown error');
+      assert.fail("The vote should have thrown an error");
     } catch (error) {
-      assert.ok(error.message.includes('revert'), 'incorrect error message');
+      assert.include(error.message, "invalid bytes32 value", "The error message should mention an invalid bytes32 value");
+    }
+  });
+
+  it("should not allow a voter to vote multiple times", async () => {
+    const contract = await NftVoting.new();
+    const nftHash = "0xabcdef0123456789abcdef0123456789";
+
+    await contract.vote(nftHash);
+
+    try {
+      await contract.vote(nftHash);
+      assert.fail("The vote should have thrown an error");
+    } catch (error) {
+      assert.include(error.message, "voter has already voted", "The error message should mention that the voter has already voted");
     }
   });
 });
-
