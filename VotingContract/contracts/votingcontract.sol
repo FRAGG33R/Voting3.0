0a49cf57487ce5555d3a90a0d2ae261d1a00050b
Abdelkarim Er-razki <aer-razk@e1r13p5.1337.ma>
2022-12-23 14:08:52 +0100

    hashes args from address to bytes32 and making a tester for voting contract

--- VotingContract/contracts/votingcontract.sol
+++ VotingContract/contracts/votingcontract.sol
@@ -1,17 +1,18 @@
 pragma solidity >=0.5.0 <0.6.0;
 
 contract NftVoting {
-	
-	mapping(address => uint) nftVotesCount;
+
+	mapping(bytes32 => uint) nftVotesCount;
 	mapping(address => uint) voterCount;
 
-	function vote(address memory _nftHash) {
-		assert(voterCount[msg.sender] == 0, "Voter already voted");
+	function vote(bytes32 _nftHash) public {
+		require(voterCount[msg.sender] == 0);
 		nftVotesCount[_nftHash]++;
 		voterCount[msg.sender]++;
 	}
 
-	function getNumberVotes(address memory _nftHash) view returns(uint) {
+	function getNumberVotes(bytes32 _nftHash) public view returns(uint) {
 		return nftVotesCount[_nftHash];
 	}
+
 }
