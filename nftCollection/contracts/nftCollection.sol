d17f92c538df7014436995d121293b8832737d66
Aissam Barchil <aissambarchil75@gmail.com>
2022-12-24 14:34:46 +0100

    add nft collection smart contract that pass 2 tests/4

--- nftCollection/contracts/nftCollection.sol
+++ nftCollection/contracts/nftCollection.sol
@@ -1,11 +1,31 @@
 pragma solidity >=0.4.16 <0.9.0;
 
-contract nftCollection {
 
-	string private __collection = "QmU8atXTgMio8UkEPXjJoqQAuu4FSqAoeiq9dun47tAy8c";
+contract nftCollection
+{
+	string public		CollectionName = "eternel";
+	address public		CollectionOwner =  0x704Dd54f36A740327cfA0AddC9177479d2Bc092a;
+	struct metadata {
+		string name;
+		string IpfsHash;
+		address owner;
+	}
+	mapping(uint256 => metadata) Collection;
+	event action(uint256 _id, string _name, address _owner);
+	event send(uint256 _id, string _name, address _oldOwner, address _newOwner);
 
-	function getCollection() view public returns(string memory)
+	function mint(string memory _name, string memory _ipfsHash, address _owner, uint256 _id) public {
+		require(msg.sender == _owner, "You must be the owner");
+		require(Collection[_id].owner == address(0), "The Id already taken");
+
+		Collection[_id] = metadata(_name, _ipfsHash, _owner);
+		emit action(_id, _name, _owner);
+	}
+
+	function transfer(uint256 _id, address _to) public 
 	{
-		return(__collection);
+		require(msg.sender == Collection[_id].owner, "You must own the nft");
+		emit send(_id, Collection[_id].name, Collection[_id].owner, _to);
+		Collection[_id].owner = _to;
 	}
 }
--- nftCollection/test/nft_collection.js
+++ nftCollection/test/nft_collection.js
@@ -1,9 +1,67 @@
-const nftCollection = artifacts.require("nftCollection");
+const nftCollection = artifacts.require('nftCollection');
 
-contract("nftCollection", () => {
-    it("should return the correct collection string", async () => {
-        const contract = await nftCollection.deployed();
-        const result = await contract.getCollection();
-        assert.equal(result, "QmU8atXTgMio8UkEPXjJoqQAuu4FSqAoeiq9dun47tAy8c", "The returned collection string is incorrect");
-    });
+contract('nftCollection', (accounts) => {
+  let nft;
+  const owner = accounts[0];
+  const recipient = accounts[1];
+
+  beforeEach(async () => {
+    nft = await nftCollection.new({ from: owner });
+  });
+
+  it('should mint a new NFT', async () => {
+    const id = 1;
+    const name = 'My NFT';
+    const ipfsHash = 'Qm123';
+
+    await nft.mint(name, ipfsHash, owner, id, { from: owner });
+
+    const result = await nft.Collection(id);
+    assert.equal(result.name, name);
+    assert.equal(result.IpfsHash, ipfsHash);
+    assert.equal(result.owner, owner);
+  });
+
+  it('should transfer an NFT', async () => {
+    const id = 1;
+    const name = 'My NFT';
+    const ipfsHash = 'Qm123';
+
+    await nft.mint(name, ipfsHash, owner, id, { from: owner });
+
+    await nft.transfer(id, recipient, { from: owner });
+
+    const result = await nft.Collection(id);
+    assert.equal(result.owner, recipient);
+  });
+
+  it('should fail to mint an NFT with a taken id', async () => {
+    const id = 1;
+    const name = 'My NFT';
+    const ipfsHash = 'Qm123';
+
+    await nft.mint(name, ipfsHash, owner, id, { from: owner });
+
+    try {
+      await nft.mint(name, ipfsHash, owner, id, { from: owner });
+      assert.fail();
+    } catch (error) {
+      assert.include(error.message, 'The Id already taken');
+    }
+  });
+
+  it('should fail to transfer an NFT that the sender does not own', async () => {
+    const id = 1;
+    const name = 'My NFT';
+    const ipfsHash = 'Qm123';
+
+    await nft.mint(name, ipfsHash, owner, id, { from: owner });
+
+    try {
+      await nft.transfer(id, recipient, { from: recipient });
+      assert.fail();
+    } catch (error) {
+      assert.include(error.message, 'You must own the nft');
+    }
+  });
 });
