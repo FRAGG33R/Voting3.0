606e5bdff40c9bb9637b9930b733b085e411a8e6
Abdelkarim Er-razki <aer-razk@e1r13p5.1337.ma>
2022-12-25 18:16:03 +0100

    adding utils in components

--- src/components/buttonAlert/buttonAlert.tsx
+++ src/components/buttonAlert/buttonAlert.tsx
@@ -1,35 +1,8 @@
 import './buttonAlert.css';
 import Web3 from 'web3';
+import { AbiItem } from '../Utils';
 //including abi data type
-export type AbiType = 'function' | 'constructor' | 'event' | 'fallback';
-export type StateMutabilityType = 'pure' | 'view' | 'nonpayable' | 'payable';
 
-export interface AbiItem {
-  anonymous?: boolean;
-  constant?: boolean;
-  inputs?: AbiInput[];
-  name?: string;
-  outputs?: AbiOutput[];
-  payable?: boolean;
-  stateMutability?: StateMutabilityType;
-  type: AbiType;
-  gas?: number;
-}
-
-export interface AbiInput {
-  name: string;
-  type: string;
-  indexed?: boolean;
-  components?: AbiInput[];
-  internalType?: string;
-}
-
-export interface AbiOutput {
-  name: string;
-  type: string;
-  components?: AbiOutput[];
-  internalType?: string;
-}
 
 const web3 = new Web3('https://goerli.infura.io/v3/30927c38dcee44098e9e4a69315c4832');
 const contractAddress = "0xD2Fdb07Cbb24F631E7c444383C3b1007Cf667184";
