b4f9800c310073af82055d958edbacbb194d4fbf
Aissam Barchil <aissambarchil75@gmail.com>
2022-12-30 14:47:10 +0100

    (chore) : setup tailwindCss

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import ButtonAlert from './components/buttonAlert/buttonAlert';
 import ButtonConnect from './components/buttonConnect/buttonConnect';
-
+import './index.css'
 function App() {
 	return (
-		<div className="App">
-			<ButtonAlert></ButtonAlert>
-			<ButtonConnect></ButtonConnect>
+		<div className="w-screen h-screen">
+			{/* <ButtonAlert></ButtonAlert>
+			<ButtonConnect></ButtonConnect> */}
 		</div>
 	);
 }
